8856ab954114a3e31f13e685c66bd8a5ea41cc81
Edward John Steere <edward.steere@gmail.com>
2021-12-01 07:34:48 +0200

    Prepare for day 2

---
+++ lisp/day2.lisp
@@ -0,0 +1,19 @@
+(defpackage :day2
+  (:use
+
+   :cl-ppcre
+   :cl
+   :trivia
+   :iterate
+   :trivia.ppcre
+   :metabang-bind
+   :anaphora
+   :cl-heap
+
+   :lib)
+
+  (:shadowing-import-from :arrow-macros :->)
+  (:shadowing-import-from :arrow-macros :->>))
+
+(in-package :day2)
+
