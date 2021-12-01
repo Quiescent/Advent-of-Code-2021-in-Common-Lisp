fe57bd1257512edf55666b15717061ed86f8dc8b
Edward John Steere <edward.steere@gmail.com>
2021-12-01 07:34:34 +0200

    Solve day1 2021

--- lisp/day1.lisp
+++ lisp/day1.lisp
@@ -5,6 +5,7 @@
    :cl
    :trivia
    :iterate
+   :trivia.ppcre
    :metabang-bind
    :anaphora
    :cl-heap
@@ -15,3 +16,21 @@
   (:shadowing-import-from :arrow-macros :->>))
 
 (in-package :day1)
+
+(defun part-1 ()
+  (iter
+    (for line in (read-problem "day1.in"))
+    (for depth = (read-from-string line))
+    (for p-depth previous depth)
+    (counting (and p-depth (> depth p-depth)))))
+
+(defun part-2 ()
+  (iter
+    (for line in (read-problem "day1.in"))
+    (for depth = (read-from-string line))
+    (for p-depth previous depth)
+    (for pp-depth previous p-depth)
+    (for ppp-depth previous pp-depth)
+    (when ppp-depth
+      (counting (> (+ depth p-depth pp-depth)
+                   (+ ppp-depth pp-depth p-depth))))))
--- lisp/quiescent-advent.asd
+++ lisp/quiescent-advent.asd
@@ -13,6 +13,7 @@
   :license "BSD"
   :description "Solutions to the 2021 advent of code"
   :long-description "The advent of code is a series of programming problems that are fun.  Some poeple race to see who can solve the problems fastest.  This year, as usual, I'm joining those who want to do it quickly."
-  :depends-on (:trivia :alexandria :cl-ppcre :arrow-macros :iterate :metabang-bind :arrow-macros :anaphora :cl-heap)
+  :depends-on (:trivia :alexandria :cl-ppcre :trivia.ppcre :arrow-macros :iterate :metabang-bind :arrow-macros :anaphora :cl-heap)
   :components ((:file "day1" :depends-on ("lib"))
+               (:file "day2" :depends-on ("lib"))
                (:file "lib")))
