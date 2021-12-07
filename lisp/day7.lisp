ed3f3d4cb55e38951d1c337941c4a05271dc4595
Edward John Steere <edward.steere@gmail.com>
2021-12-07 07:18:24 +0200

    Solve day 7

--- lisp/day7.lisp
+++ lisp/day7.lisp
@@ -17,3 +17,36 @@
   (:shadowing-import-from :arrow-macros :->>))
 
 (in-package :day7)
+
+(defun spent-fuel (xs i)
+  (->> (mapcar (lambda (number) (abs (- number i))) xs)
+    (apply #'+)))
+
+(defun part-1 ()
+  (iter
+    (with numbers = (->> (read-problem "day7.in")
+                      car
+                      (split ",")
+                      (mapcar #'read-from-string)))
+    (with minimum = (apply #'min numbers))
+    (with maximum = (apply #'max numbers))
+    (for i from minimum to maximum)
+    (minimizing (spent-fuel numbers i))))
+
+(defun sum-ints-to (x)
+  (/ (* x (+ x 1)) 2))
+
+(defun real-spent-fuel (xs i)
+  (->> (mapcar (lambda (number) (sum-ints-to (abs (- number i)))) xs)
+    (apply #'+)))
+
+(defun part-2 ()
+  (iter
+    (with numbers = (->> (read-problem "day7.in")
+                      car
+                      (split ",")
+                      (mapcar #'read-from-string)))
+    (with minimum = (apply #'min numbers))
+    (with maximum = (apply #'max numbers))
+    (for i from minimum to maximum)
+    (minimizing (real-spent-fuel numbers i))))
