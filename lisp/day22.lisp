d582b77e9cd2f9321d8bc768c1a6b36f385dc7a1
Edward John Steere <edward.steere@gmail.com>
2021-12-22 13:19:15 +0200

    WIP implementing a simple interval map while thinking about solution

--- lisp/day22.lisp
+++ lisp/day22.lisp
@@ -81,20 +81,36 @@
 (defun interval-map ()
   (list))
 
+(defun intersects (start end istart iend)
+  (or (<= start istart end)
+      (<= start iend   end)
+      (<= start istart iend end)
+      (<= istart start end iend)))
+
 (defun find-intersecting (interval-map start end)
   (assert (>= end start))
   (iter
     (for cell on interval-map)
     (for p-interval previous cell)
     (for (istart iend x) = (car cell))
-    (finding (list p-interval cell (cdr cell))
-             :such-that (or (<= start istart end)
-                            (<= start iend   end)
-                            (<= start istart iend end)
-                            (<= istart start end iend)))))
+    (finding (list p-interval cell)
+             :such-that (intersects start end istart iend))))
 
 (defun sort-intervals (interval-map)
   (sort interval-map #'< :key #'car))
+
+(defun collect-full-intersection (interval-map start end)
+  (assert (>= end start))
+  (iter
+    (for cell on interval-map)
+    (for (istart iend x) = (car cell))
+    (while (intersects start end istart iend))
+    (collecting x into xs)
+    (maximizing iend into max-iend)
+    (minimizing istart into min-istart)
+    (finally (return (if (intersects start end istart iend)
+                         (list (cdr cell) min-istart max-iend xs)
+                         (list cell min-istart max-iend xs))))))
 
 (defun insert-range-with (interval-map start end fn)
   (assert (>= end start))
@@ -102,12 +118,12 @@
       (list (list start end (funcall fn nil)))
       (bind ((intersected (find-intersecting interval-map start end)))
         (if intersected
-            (bind (((previous intersection next) intersected)
-                   ((istart iend x)              (car intersection))
-                   (new-cell                     (cons (list (min istart start)
-                                                             (max iend   end)
-                                                             (funcall fn x))
-                                                       next)))
+            (bind (((previous intersection) intersected)
+                   ((next istart iend xs)   (collect-full-intersection intersection start end))
+                   (new-cell                (cons (list (min istart start)
+                                                        (max iend   end)
+                                                        (funcall fn xs))
+                                                  next)))
               (if previous
                   (setf (cdr previous) new-cell)
                   (setf interval-map   new-cell)))
@@ -115,6 +131,20 @@
                   (cons (list start end (funcall fn nil))
                         (cdr interval-map))))
         (sort-intervals interval-map))))
+
+(defun delete-range-with (interval-map start end fn)
+  (assert (>= end start))
+  (if (null interval-map)
+      interval-map
+      (bind ((intersected (find-intersecting interval-map start end)))
+        (if (not intersected)
+            interval-map
+            (bind (((previous intersection) intersected)
+                   ((next istart iend xs)   (collect-full-intersection intersection start end))
+                   (result                  (funcall fn istart iend xs)))
+              (if previous
+                  (setf (cdr previous) (if result (cons result next) next))
+                  (setf interval-map   (if result (cons result next) next))))))))
 
 #+nil
 (-> (interval-map)
@@ -129,8 +159,30 @@
   (insert-range-with 10 12 (lambda (x) (if (null x) 'z (cons x 'z))))
   (insert-range-with -1 1  (lambda (x) (if (null x) 'a (cons x 'a)))))
 
+#+nil
+(-> (interval-map)
+  (insert-range-with 2 4    (lambda (x) (if (null x) 'x (cons x 'x))))
+  (insert-range-with 4 8    (lambda (x) (if (null x) 'y (cons x 'y))))
+  (insert-range-with 10 12  (lambda (x) (if (null x) 'z (cons x 'z))))
+  (insert-range-with -1 13  (lambda (x) (if (null x) 'a (cons x 'a)))))
+
+#+nil
+(-> (interval-map)
+  (insert-range-with 2 4    (lambda (x) (if (null x) 'x (cons x 'x))))
+  (insert-range-with 4 8    (lambda (x) (if (null x) 'y (cons x 'y))))
+  (insert-range-with 10 12  (lambda (x) (if (null x) 'z (cons x 'z))))
+  (delete-range-with 1 3    (lambda (start end values) nil)))
+
 (defun part-2 ()
   (bind ((instructions (parse-problem)))
-    ))
+    (iter
+      (with interval-map = (interval-map))
+      (for (op x-start
+               x-end
+               y-start
+               y-end
+               z-start
+               z-end) in instructions)
+      ())))
 
 ;; 870227697432749 too low!
