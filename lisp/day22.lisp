81238268f3254c8c6a9eeed2b1cd08120c472cd3
Edward John Steere <edward.steere@gmail.com>
2021-12-23 07:06:37 +0200

    Finish day 22 cleanup

--- lisp/day22.lisp
+++ lisp/day22.lisp
@@ -75,53 +75,38 @@
            (counting value)))))))
 
 (defun intersecting-area (box-1 box-2)
-  (bind (((op-1 x-start-1 x-end-1 y-start-1 y-end-1 z-start-1 z-end-1 . rest) box-1)
-         ((op-2 x-start-2 x-end-2 y-start-2 y-end-2 z-start-2 z-end-2 . rest) box-2))
+  (bind (((x-start-1 x-end-1 y-start-1 y-end-1 z-start-1 z-end-1) box-1)
+         ((x-start-2 x-end-2 y-start-2 y-end-2 z-start-2 z-end-2) box-2))
     (* (max (1+ (- (min x-end-1 x-end-2) (max x-start-1 x-start-2))) 0)
        (max (1+ (- (min y-end-1 y-end-2) (max y-start-1 y-start-2))) 0)
        (max (1+ (- (min z-end-1 z-end-2) (max z-start-1 z-start-2))) 0))))
 
 (defun intersecting-box (box-1 box-2)
-  (bind (((op-1 x-start-1 x-end-1 y-start-1 y-end-1 z-start-1 z-end-1 . rest) box-1)
-         ((op-2 x-start-2 x-end-2 y-start-2 y-end-2 z-start-2 z-end-2 . rest) box-2))
+  (bind (((x-start-1 x-end-1 y-start-1 y-end-1 z-start-1 z-end-1) box-1)
+         ((x-start-2 x-end-2 y-start-2 y-end-2 z-start-2 z-end-2) box-2))
     (and (> (intersecting-area box-1 box-2) 0)
-         (list (list op-1 op-2)
-               (max x-start-1 x-start-2)
+         (list (max x-start-1 x-start-2)
                (min x-end-1   x-end-2)
                (max y-start-1 y-start-2)
                (min y-end-1   y-end-2)
                (max z-start-1 z-start-2)
-               (min z-end-1   z-end-2)
-               box-2))))
+               (min z-end-1   z-end-2)))))
 
 (defun areas-seen (box boxes-added)
   (bind ((overlaps (->> (mapcar (lambda (other-box) (intersecting-box box other-box))
                                 boxes-added)
                      (remove nil))))
-    ;; (format t "overlaps: ~a~%" overlaps)
     (if overlaps
         (iter
           (for tail on overlaps)
           (for overlap = (car tail))
-          (for ((op-1 op-2) x-start x-end y-start y-end z-start z-end other-box) = overlap)
-          (when (and (eq op-1 op-2) (eq op-1 'off))
-            (next-iteration))
-          (if (eq op-2 'off)
-              (progn
-                (format t "here!~%")
-                (summing (- (* (1+ (- x-end x-start))
-                                (1+ (- y-end y-start))
-                                (1+ (- z-end z-start))))
-                         :into overlap-area)
-                (summing (- (areas-seen other-box (cdr tail)) (areas-seen overlap (cdr tail)))
-                         :into overlap-area-seen))
-              (progn
-                (summing (* (1+ (- x-end x-start))
-                            (1+ (- y-end y-start))
-                            (1+ (- z-end z-start)))
-                         :into overlap-area)
-                (summing (areas-seen overlap (cdr tail))
-                         :into overlap-area-seen)))
+          (for (x-start x-end y-start y-end z-start z-end) = overlap)
+          (summing (* (1+ (- x-end x-start))
+                      (1+ (- y-end y-start))
+                      (1+ (- z-end z-start)))
+                   :into overlap-area)
+          (summing (areas-seen overlap (cdr tail))
+                   :into overlap-area-seen)
           (finally (return (- overlap-area overlap-area-seen))))
         0)))
 
@@ -131,8 +116,8 @@
         (for instruction in on-instructions)
         (bind ((intersection (intersecting-box instruction off-instruction)))
           (if intersection
-              (bind (((op-i x-start-i x-end-i y-start-i y-end-i z-start-i z-end-i . rest) intersection)
-                     ((op   x-start   x-end   y-start   y-end   z-start   z-end   . rest) instruction))
+              (bind (((x-start-i x-end-i y-start-i y-end-i z-start-i z-end-i) intersection)
+                     ((x-start   x-end   y-start   y-end   z-start   z-end)   instruction))
                 (cond
                   ;; on command inside of off command: don't collect any cubes!
                   ((and (>= x-start x-start-i)
@@ -149,38 +134,32 @@
                   ;; afterwards!!
                   (t
                    ;; top slab
-                   (collecting (list 'on
-                                     x-start x-end
+                   (collecting (list x-start x-end
                                      y-start y-end
                                      z-start (1- z-start-i)))
                    ;; bottom slab
-                   (collecting (list 'on
-                                     x-start      x-end
+                   (collecting (list x-start      x-end
                                      y-start      y-end
                                      (1+ z-end-i) z-end))
                    ;; left projected face
-                   (collecting (list 'on
-                                     x-start  (1- x-start-i)
+                   (collecting (list x-start  (1- x-start-i)
                                      y-start-i y-end-i
                                      z-start-i z-end-i))
                    ;; right projected face
-                   (collecting (list 'on
-                                     (1+ x-end-i) x-end
+                   (collecting (list (1+ x-end-i) x-end
                                      y-start-i    y-end-i
                                      z-start-i    z-end-i))
                    ;; front sliver
-                   (collecting (list 'on
-                                     x-start   x-end
+                   (collecting (list x-start   x-end
                                      y-start   (1- y-start-i)
                                      z-start-i z-end-i))
                    ;; back sliver
-                   (collecting (list 'on
-                                     x-start      x-end
+                   (collecting (list x-start      x-end
                                      (1+ y-end-i) y-end
                                      z-start-i    z-end-i)))))
               (collecting instruction))))
     (remove-if (lambda (instruction)
-                 (bind (((op x-start x-end y-start y-end z-start z-end . rest) instruction))
+                 (bind (((x-start x-end y-start y-end z-start z-end) instruction))
                    (or (< x-end x-start)
                        (< y-end y-start)
                        (< z-end z-start)))))))
@@ -190,37 +169,34 @@
     (with processed = (list))
     (for instruction in instructions)
     (if (eq (car instruction) 'on)
-        (push instruction processed)
-        (setf processed (turn-off-regions instruction processed)))
+        (push (cdr instruction) processed)
+        (setf processed (turn-off-regions (cdr instruction) processed)))
     (finally (return processed))))
 
 (defun part-2 ()
   (bind ((instructions (preprocess (parse-problem)))
          (boxes-added  (list)))
     (iter
-      (for (op x-start
-               x-end
-               y-start
-               y-end
-               z-start
-               z-end) in instructions)
+      (for (x-start
+            x-end
+            y-start
+            y-end
+            z-start
+            z-end) in instructions)
       ;; For all the boxs we already have, remove the intersecting
       ;; boxes from this one by intersecting with all of the removed
       ;; boxes and then adding to the list of removed boxes.
-      (bind ((current-box (list op
-                                x-start
+      (bind ((current-box (list x-start
                                 x-end
                                 y-start
                                 y-end
                                 z-start
                                 z-end))
              (areas-seen (areas-seen current-box boxes-added)))
-        (if (eq op 'on)
-            (summing (- (* (1+ (- x-end x-start))
-                           (1+ (- y-end y-start))
-                           (1+ (- z-end z-start)))
-                        areas-seen))
-            (summing (- areas-seen)))
+        (summing (- (* (1+ (- x-end x-start))
+                       (1+ (- y-end y-start))
+                       (1+ (- z-end z-start)))
+                    areas-seen))
         (push current-box boxes-added)))))
 
 ;; 870227697432749 too low!
